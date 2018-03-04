48a139479730514da229135687370c3cc2c0c503
Strovala <strahinja.kovacevic994@gmail.com>
2018-03-04 19:55:57 +0100

    Added socket io server

--- xo/xo.py
+++ xo/xo.py
@@ -1,6 +1,5 @@
 from flask import Flask, send_from_directory, render_template, request, json
-from flask_socketio import SocketIO
-
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -17,5 +16,33 @@ def start():
     return render_template('game.html', message=body.get('name'))
 
 
+@socketio.on('connect')
+def connect_handler():
+    emit('connect',
+         {'message': '{0} has joined'},
+         broadcast=True)
+
+
+@socketio.on('play')
+def handle_my_custom_event(data):
+    print('received json: ' + str(data))
+
+
 if __name__ == '__main__':
-    socketio.run()
+    socketio.run(app)
+
+
+
+# # Object that represents a socket connection
+# class Socket:
+#     def __init__(self, sid):
+#         self.sid = sid
+#         self.connected = True
+#
+#     # Emits data to a socket's unique room
+#     def emit(self, event, data):
+#         emit(event, data, room=self.sid)
+#
+# @socketio.on('connect')
+# def foo():
+#     sockets[request.sid] = Socket(request.sid)
